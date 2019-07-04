980c5a8c12be6ebe6a5eeff9853696a819d5de22
mangattan <tanianainggolan00@gmail.com>
2019-07-04 12:45:55 +0700

    Upload kedua

---
+++ If Else/ifelse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a,b;
+	
+	cout<<"Input Angka I: ";
+	cin>>a;
+	
+	cout<<"\nInput Angka II : ";
+	cin>>b;
+	
+	if(a>b){
+		cout<<"Angka I Lebih Besar dari Angka II" ;
+	} else{
+		cout<<"EROR";
+	}
+	
+	return 0;
+}
